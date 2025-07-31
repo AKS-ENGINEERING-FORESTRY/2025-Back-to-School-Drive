a1b7831c46c665536e7965d1f6610042479b4492
tylerg-aks <tyler.goodell@aks-eng.com>
2025-07-31 15:01:44 -0700

    Add files via upload
    Totals set to 0.    7/31/25 3pm update

--- sketch.js
+++ sketch.js
@@ -1,8 +1,8 @@
 let items = [
-  { name: "BACKPACKS", donated: 35, goal: 50, color: "#d4145a", icon: "backpack.png" },
-  { name: "WIRED EARBUDS", donated: 10, goal: 30, color: "#fbb03b", icon: "earbuds.png" },
-  { name: "WIRED OVER-THE-EAR\nHEADPHONES", donated: 14, goal: 20, color: "#22b573", icon: "headphones.png" },
-  { name: "WATER BOTTLES", donated: 22, goal: 30, color: "#29abe2", icon: "waterbottle.png" }
+  { name: "BACKPACKS", donated: 0, goal: 50, color: "#d4145a", icon: "backpack.png" },
+  { name: "WIRED EARBUDS", donated: 0, goal: 30, color: "#fbb03b", icon: "earbuds.png" },
+  { name: "WIRED OVER-THE-EAR\nHEADPHONES", donated: 0, goal: 20, color: "#22b573", icon: "headphones.png" },
+  { name: "WATER BOTTLES", donated: 0, goal: 30, color: "#29abe2", icon: "waterbottle.png" }
 ];
 
 let icons = [];
@@ -10,7 +10,7 @@ let legend;
 
 
 let moneyIcon;
-let moneyChart = { donated: 1000, goal: 2000, color: "#22b573", icon: "money.png" };
+let moneyChart = { donated: 0, goal: 2000, color: "#22b573", icon: "money.png" };
 
 
 function preload() {
