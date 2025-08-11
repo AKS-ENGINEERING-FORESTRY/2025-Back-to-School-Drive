f62169926131b954dd17779d39bfd97bdc08e695
tylerg-aks <tyler.goodell@aks-eng.com>
2025-08-11 11:06:39 -0700

    Update sketch.js
    Money Update 8/11/25 11am

--- sketch.js
+++ sketch.js
@@ -10,7 +10,7 @@ let legend;
 
 
 let moneyIcon;
-let moneyChart = { donated: 2080, goal: 2000, color: "#22b573", icon: "money.png" };
+let moneyChart = { donated: 2265, goal: 2000, color: "#22b573", icon: "money.png" };
 
 
 function preload() {
